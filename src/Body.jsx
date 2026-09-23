902f3108bea6fd267b4abd7ff9288c3dc3e455e9
yogesh1104 <yogeshgupta8982@gmail.com>
2026-09-23 18:23:00 +0530

    Dev Tinder Web

---
+++ src/Body.jsx
@@ -0,0 +1,15 @@
+import NavBar from './Navbar'
+import { Outlet } from 'react-router-dom'
+import Footer from './Footer'
+
+const Body = () => {
+    return (
+    <div>
+        <NavBar />
+        <Outlet />
+        <Footer />
+    </div>
+    )
+}
+
+export default Body
